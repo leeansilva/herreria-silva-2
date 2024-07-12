96e0d817e086fc2d062c8c06673f07f8451e195e
Lean <leeansilva@gmail.com>
2024-07-12 15:03:07 -0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import CaptionCarousel from "@/components/Carousel/Carousel";
 import Hero from "@/components/Hero/Hero";
-import QuienesSomos from "@/components/QuienesSomos/QuienesSomos";
+import QuienesSomos from "@/components/QuienesSomos/quienesSomos";
 import { Box } from "@chakra-ui/react";
 import { Link, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 
